4713be19debc2b89af5ce06c68447f20b3fa9c4c
pashacyborg <pashacyborg2@gmail.com>
2022-12-06 14:34:12 +0300

    gh-pages

--- Practice 7/practice-7/src/End/End.js
+++
@@ -1,9 +0,0 @@
-import './End.css';
-
-export function End () {
-    return (
-        <div id="position2">
-            <label id="end">2021 © Все права и планета защищены</label>
-        </div>
-    );
-}
